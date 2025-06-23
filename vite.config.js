fd3602f5d749d5c19558e208119d803d8ad3359f
Javixx Xd <javier.vergara@virginiogomez.cl>
2025-06-23 14:32:12 -0400

    Configuración para GitHub Pages con Vite

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/trabajo de la evaluación 3 de diseño web/'  // <— Muy importante
+})
